a4cf444bcb3d26c48f629c98b2ae986381e39ff9
rijan7ghimire <55038884+rijan7ghimire@users.noreply.github.com>
2020-01-16 22:56:11 +0545

    Update lab5quest2.c

--- lab5/lab5quest2.c
+++ lab5/lab5quest2.c
@@ -5,24 +5,24 @@ void main()
     printf("Enter two numbers:");
     scanf("%d%d",&a,&b);
     if (a>b){
-    for (i=1 ;i<=a;i++)
+    for (i=a ;i>=1;i--)
     {
         if (a%i==0 && b%i==0)
         {
             printf("The HCF is %d \n",i);
             printf("The LCM is %d",(a*b)/i);
-            i=a+1;
+            i=0;
         }
     }
     }
      else if (a>b){
-    for (i=1 ;i<=b;i++)
+    for (i=b ;i>=1;i--)
     {
         if (a%i==0 && b%i==0)
         {
             printf("The HCF is %d \n",i);
             printf("The LCM is %d",(a*b)/i);
-            i=b+1;
+            i=0;
         }
     }
     }
